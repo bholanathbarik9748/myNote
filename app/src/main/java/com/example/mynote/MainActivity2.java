9277df30f5c692b12c519d0171765870d5efcdc8
Bolanath <bholanathbarik9748@gmail.com>
2021-02-05 01:12:57 +0530

    set responsiveness and crete delete and add note button and its separately work and immprove docs (complete project)

--- app/src/main/java/com/example/mynote/MainActivity2.java
+++ app/src/main/java/com/example/mynote/MainActivity2.java
@@ -1,17 +1,23 @@
 package com.example.mynote;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -27,6 +33,18 @@ public class MainActivity2 extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.add_note) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return false;
+    }
+
     //    auto topic selector
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +52,15 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         ListView list = (ListView) findViewById(R.id.listView);
-        notes.add("BB");
+        SharedPreferences sp = getApplicationContext().getSharedPreferences("com.example.mynote", Context.MODE_PRIVATE);
+        HashSet<String> set  = (HashSet<String>) sp.getStringSet("Note",null);
+        if(set == null){
+            notes.add("Empty!!!!!");
+        }else{
+            notes = new ArrayList(set);
+        }
+
+
 
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notes);
         list.setAdapter(arrayAdapter);
@@ -45,6 +71,28 @@ public class MainActivity2 extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
                 intent.putExtra("a", position);
                 startActivity(intent);
+            }
+        });
+
+//        Delete Button
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                new AlertDialog.Builder(MainActivity2.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you Sure?")
+                        .setMessage("Do you really want do delete this Note ?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                notes.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+                                SharedPreferences sp = getApplicationContext().getSharedPreferences("com.example.mynote", Context.MODE_PRIVATE);
+                                HashSet<String> set  = new HashSet(MainActivity2.notes);
+                                sp.edit().putString("notes", String.valueOf(set)).apply();
+                            }
+                        }).setNegativeButton("No", null).show();
+                return true;
             }
         });
     }
--- app/src/main/java/com/example/mynote/MainActivity3.java
+++ app/src/main/java/com/example/mynote/MainActivity3.java
@@ -1,6 +1,8 @@
 package com.example.mynote;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,7 +10,10 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashSet;
+
 public class MainActivity3 extends AppCompatActivity {
+    int tempholder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,14 @@ public class MainActivity3 extends AppCompatActivity {
 //        Id
         EditText editText = (EditText) findViewById(R.id.editText);
         Intent intent = getIntent();
-        final int tempholder = intent.getIntExtra("a", -1);
+        tempholder = intent.getIntExtra("a", -1);
 
         if (tempholder != -1) {
             editText.setText(MainActivity2.notes.get(tempholder));
+        } else {
+            MainActivity2.notes.add("");
+            tempholder = MainActivity2.notes.size() - 1;
+            MainActivity2.arrayAdapter.notifyDataSetChanged();
         }
 
         editText.addTextChangedListener(new TextWatcher() {
@@ -34,6 +43,9 @@ public class MainActivity3 extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 MainActivity2.notes.set(tempholder, String.valueOf(s));
                 MainActivity2.arrayAdapter.notifyDataSetChanged();
+                SharedPreferences sp = getApplicationContext().getSharedPreferences("com.example.mynote", Context.MODE_PRIVATE);
+                HashSet<String> set  = new HashSet(MainActivity2.notes);
+                sp.edit().putString("notes", String.valueOf(set)).apply();
             }
 
             @Override
